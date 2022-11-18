0a40380621678714695e780768e5d7f454cf4de0
rajeshdev-net <82808035+rajeshdev-net@users.noreply.github.com>
2022-11-18 17:52:10 +0530

    Add files via upload

---
+++ CastleWindsorTest/CastleWindsorTest/BAL/HigherBusiness.cs
@@ -0,0 +1,46 @@
+﻿using CastleWindsorTest.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleWindsorTest.BAL
+{
+    public interface IHigherBusiness
+    {
+        void DoSomething();
+    }
+
+    public class HigherBusiness : IHigherBusiness
+    {
+        private ISomeBusiness someBusiness;
+
+        public HigherBusiness(ISomeBusiness someBusiness)
+        {
+            this.someBusiness = someBusiness;
+        }
+        public void DoSomething()
+        {
+            someBusiness.DoSomething();
+        }
+    }
+
+    public interface ISomeBusiness
+    {
+        void DoSomething();
+    }
+
+    public class Business : ISomeBusiness
+    {
+        private ILogger logger;
+        public Business(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void DoSomething()
+        {
+            logger.Log("Message");
+        }
+    }
+}
